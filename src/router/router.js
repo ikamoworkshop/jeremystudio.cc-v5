510ed6ff05652a2a8f11228c9c3661fe8673c88a
Jeremy Chang <changjeremy0226@gmail.com>
2025-03-17 00:08:59 -0700

    Setup router plugin

---
+++ src/router/router.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routs = [
+
+]
+
+const router = createRouter({
+    routes,
+    history: createWebHistory()
+})
+
+export default router
